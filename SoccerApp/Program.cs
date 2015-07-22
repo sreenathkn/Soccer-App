823d06bca385f00434bf327ae296092b4893fe6a
sudheesh <Sudheesh@beesys.com>
2015-07-22 14:40:07 +0530

    Issue #1

--- SoccerApp/Program.cs
+++ SoccerApp/Program.cs
@@ -17,6 +17,7 @@ namespace SoccerApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Issue #1
         }
     }
 }
