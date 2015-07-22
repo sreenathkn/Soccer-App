ef5424b8d73b205efc3ef9d9693d2817b118671f
sudheesh <Sudheesh@beesys.com>
2015-07-22 11:23:15 +0530

    Implemented default selection

--- Controller/MatchPart.cs
+++ Controller/MatchPart.cs
@@ -26,8 +26,15 @@ namespace Controller
             listBox1.DataSource = t;
             listBox1.DisplayMember = "Name";
             listBox1.ValueMember = "ID";
-        }
+            DataRow[] dr = _objUDT.CurrentDataSet.Tables[11].Select("Active=true");
+            if (dr.Count() > 0)
+            {
+                int index = listBox1.FindString(dr[0]["Name"].ToString());
+                if (index != -1)
+                    listBox1.SetSelected(index, true);
 
+            }
+        }
         private void listBox1_DoubleClick(object sender, EventArgs e)
         {
             _objController.selectedMatchPart = listBox1.Text;
@@ -39,6 +46,17 @@ namespace Controller
                 _objController.Udt.UdtFilters.Add("Match Part", filter);
             else
                 _objController.Udt.UdtFilters["Match Part"] = filter;
+
+            foreach (DataRowView item in listBox1.Items)
+            {
+                System.Diagnostics.Trace.WriteLine(item["Name"].ToString());
+
+                if (item["Name"].ToString() == listBox1.Text)
+                    _objUDT.UpdateUDT(11, new string[] { "Active" }, new string[] { "true" }, "Name", listBox1.Text);
+                else
+                    _objUDT.UpdateUDT(11, new string[] { "Active" }, new string[] { "false" }, "Name", item["Name"].ToString());
+            }
+
             this.Close();
         }
     }
