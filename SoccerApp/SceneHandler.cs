71a2d9fa4ea6da125beb5ec456ba9eaaa7ed1e82
sudheesh <Sudheesh@beesys.com>
2015-08-18 12:20:39 +0530

    New file for handling scene through sdhotbox #7

---
+++ SoccerApp/SceneHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SoccerApp
+{
+   public class SceneHandler
+    {
+       public bool isInitialized { get; set; }
+       public bool isSceneLoaded { get; set; }
+       public bool Initialize()
+       {
+           isInitialized = false;
+           if (ConfigurationManager.AppSettings["w3dscene"] != null)
+           {
+               if(File.Exists(ConfigurationManager.AppSettings["w3dscene"]))
+               {
+                   isInitialized = true;
+
+               }
+           }
+           
+           return isInitialized;
+       }
+       public void LoadScene()
+       {
+           isSceneLoaded = false;
+           if(isInitialized)
+           {
+
+           }
+
+       }
+       public void UpdateScore(string Team,string score)
+       {
+           if(isInitialized && isSceneLoaded)
+           {
+
+           }
+       }
+       public void TimerAction(string actiontype,string counter)
+       {
+           if (isInitialized && isSceneLoaded)
+           {
+
+           }
+       }
+    }
+}
